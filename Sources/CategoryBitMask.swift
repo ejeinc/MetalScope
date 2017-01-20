3c3fc3c89243edfb3d00f868fd22cf828ddb1ab4
Jun Tanaka <junpluse@gmail.com>
2017-01-20 21:00:45 +0900

    Add CategoryBitMask struct

---
+++ Sources/CategoryBitMask.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryBitMask.swift
+//  Axel
+//
+//  Created by Jun Tanaka on 2017/01/20.
+//  Copyright © 2017 eje Inc. All rights reserved.
+//
+
+import SceneKit
+
+public struct CategoryBitMask: OptionSet {
+	public var rawValue: Int
+
+	public init(rawValue: Int) {
+		self.rawValue = rawValue
+	}
+}
+
+extension CategoryBitMask {
+	public static let all = CategoryBitMask(rawValue: .max)
+	public static let leftEye = CategoryBitMask(rawValue: 1 << 21)
+	public static let rightEye = CategoryBitMask(rawValue: 1 << 22)
+}
