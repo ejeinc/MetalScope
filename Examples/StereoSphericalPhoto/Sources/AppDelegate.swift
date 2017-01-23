3c3800989709d52bfe55395e626250a3a902be46
Jun Tanaka <junpluse@gmail.com>
2017-01-23 23:49:24 +0900

    Update StereoSphericalPhoto example

--- Examples/StereoSphericalPhoto/Sources/AppDelegate.swift
+++ Examples/StereoSphericalPhoto/Sources/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 		window = UIWindow(frame: UIScreen.main.bounds)
+		window?.tintColor = .white
 		window?.rootViewController = ViewController()
 		window?.makeKeyAndVisible()
 		return true
